d57ff57ccebadd927b4edd59abd3afded2e657fb
stephen-ross2 <stephen.ross2@outlook.com>
2024-12-03 08:50:34 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Assignment_5_2_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string to determine the length of the last word: ");
+            string input = Console.ReadLine();
+
+            int lengthOfLastWord = LengthOfLastWord(input);
+            Console.WriteLine($"The length of the last word is: {lengthOfLastWord} characters");
+        }
+        
+        public static int LengthOfLastWord(string s)
+        {
+            int length = 0;
+            int i = s.Length - 1;
+            while (i >= 0 && s[i] == ' ')
+            {
+                i--;
+            }
+            while (i >= 0 && s[i] != ' ')
+            {
+                length++;
+                i--;
+            }
+            return length;
+        }
+    }
+}
+
